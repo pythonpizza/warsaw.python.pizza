f18844acae72a92ee428e3d1b88eba71a466c831
Piotr Grędowski <piotrgredowski@gmail.com>
2026-03-27 09:43:11 +0100

    fix(venue): correct venue name to PJAIT

--- src/data/generic.ts
+++ src/data/generic.ts
@@ -2,7 +2,7 @@ export const genericInformation = {
   pageTitle: "Warsaw Python Pizza",
   socialDescription: "Warsaw Python Pizza, join us in Warsaw on 9th May 2026",
   siteHeading: "Warsaw Python Pizza",
-  subHeading: "PJATK building A, Koszykowa 86, Warsaw, Poland",
+  subHeading: "PJAIT building A, Koszykowa 86, Warsaw, Poland",
   whenDatetime: "2026/05/09 10:00 AM UTC+2",
   whenFormat: "do MMMM y",
 };
--- src/sections/venue.tsx
+++ src/sections/venue.tsx
@@ -4,7 +4,7 @@ export const Venue = () => (
   <Container id="venue" variant="smallContainer">
     <Heading>Venue</Heading>
     <Paragraph>
-      📍 PJATK building A, Auditorium, Koszykowa 86, Warsaw, Poland
+      📍 PJAIT building A, Auditorium, Koszykowa 86, Warsaw, Poland
     </Paragraph>
   </Container>
 );
